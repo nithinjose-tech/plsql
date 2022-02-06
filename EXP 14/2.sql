4385f2b1290d9d5b8226f3b2313edbef6fb8b53d
Nithin Jose <ninjos8@gmail.com>
2022-02-06 17:15:22 +0530

    Update 2.sql

--- EXP 14/2.sql
+++ EXP 14/2.sql
@@ -1,3 +1,22 @@
+
+
+-- CREATE TABLE Department(dept_name VARCHAR(20),building VARCHAR (50),budget INT,PRIMARY KEY (dept_name));
+
+-- CREATE TABLE Instructor(ID int,name VARCHAR(20),dept_name VARCHAR(20),salary int,PRIMARY KEY (ID),FOREIGN KEY (dept_name) REFERENCES Department(dept_name));
+
+-- INSERT INTO Department VALUES('Biology','Watson',90000);
+-- INSERT INTO Department VALUES('Comp.Sci','Taylor',100000);
+-- INSERT INTO Department VALUES('Elec.Eng','Taylor',85000);
+-- INSERT INTO Department VALUES('Finance','Painter',120000);
+-- INSERT INTO Department VALUES('History','Painter',50000);
+-- INSERT INTO Department VALUES('Music','Packard',80000);
+-- INSERT INTO Department VALUES('Physics','Watson',70000);
+
+
+-- INSERT INTO Instructor VALUES(10101,'Srinivasan','Comp.Sci',65000);
+-- INSERT INTO Instructor VALUES(12121,'Wu','Finance',90000);
+
+
 -- CREATE TABLE STATUS
 -- (
 --     new integer,
@@ -16,11 +35,11 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('SAL DIFF = ' || abs(:OLD.SALARY - :NEW.SALARY));
 END;
 
-
 -- UPDATE INSTRUCTOR
 -- SET SALARY = SALARY * 1.1
 -- WHERE NAME = 'Srinivasan';
 -- /
+
 
 
 -- select * from STATUS;
