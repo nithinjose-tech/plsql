c9b8b67ae410fb3f4c875d8f42b878d982af48c3
Nithin Jose <ninjos8@gmail.com>
2022-02-06 16:37:44 +0530

    Update area.sql

--- EXP 13/area.sql
+++ EXP 13/area.sql
@@ -1,3 +1,5 @@
+
+
 declare
     r number;
     a number;
@@ -12,26 +14,24 @@ declare
 begin
     dbms_output.put_line('All Units in cm2');
     
-    r := 4;
+    r := 2;
     area.area_circle(r,a);
     dbms_output.put_line('Area of Circle of Radius ' || r || ' is: ' || a || ' cm2');
     
-    b := 4;
-    h := 5;
+    b := 3;
+    h := 4;
     area.area_right_angled(b,h,a);
     dbms_output.put_line('Area of Right Angled Triangle of Base ' || b || ' and height ' || h || ' is: ' || a|| ' cm2');
     
-    a1 := 5;
-    b1 := 8;
-    c1 := 10;
+    a1 := 3;
+    b1 := 4;
+    c1 := 5;
     a := area.area_triangle(a1,b1,c1);
     dbms_output.put_line('Area of Triangle with Sides ' || a1 || ', ' || b1 || ', ' || c1 || ' is: ' || ROUND(a,2) || ' cm2');
     
-    r1 := &radius_cylinder;
-    h1 := &height_cylinder;
+    r1 := 8;
+    h1 := 9;
     a := area.volume(r1,h1);
     dbms_output.put_line('Volume of Cylinder with Radius ' || r1 || ' and height ' || h1 || ' is: ' || a || ' cm2');
 end;
 /
-
-
